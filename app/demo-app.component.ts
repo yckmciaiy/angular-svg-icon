a2ea31acc3145b3ea1effa0988e0614b3aa1a183
David Czeck <david@ibiblio.org>
2017-07-26 00:56:20 -0700

    Move styles into css

--- app/demo-app.component.ts
+++ app/demo-app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SvgIconRegistryService } from 'angular-svg-icon/index';
+
 
 @Component({
 	selector: 'demo-app',
-	styles : [ 'fieldset input { margin-right: 10px; }' ],
+	styleUrls: [ 'app/demo-app.component.css' ],
 	templateUrl: 'app/demo-app.component.html'
 })
 
@@ -10,9 +12,19 @@ export class DemoAppComponent {
 	private r = 120;
 	private g = 120;
 	private b = 120;
-	private w = 75;
+	private w = 175;
+
+	display = true;
+
+	constructor(private registry:SvgIconRegistryService) {
+	}
 
 	getStyle(): string {
 		return 'width:' + this.w + 'px;fill:rgb(' + this.r + ',' + this.g + ',' + this.b + ');';
 	}
+
+	unload(url:string) {
+		this.registry.unloadSvg(url);
+	}
+
 }
